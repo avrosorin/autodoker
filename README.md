351e4dcbb9bda00f96f3470e209933b3a26e665c
sorin-broscaru-casumo <155720784+sorin-broscaru-casumo@users.noreply.github.com>
2024-03-02 00:18:17 +0200

    Run docker compose if present

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # autodoker
+
+A docker image to build and deploy private repos on Portainer
--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,12 @@ repo_name=$(basename "$repository" .git)
 container_port="${rPORT:-3000}"
 network="${NETWORK:-reverseproxy}"
 
+# Check if Docker Compose is available
+if ! command -v docker-compose >/dev/null 2>&1; then
+    echo "Docker Compose is not installed. Please install Docker Compose and try again."
+    exit 1
+fi
+
 # SWITCH WORKING DIR
 cd "/app" || { echo "Failed to change to repository directory"; exit 1; }
 
@@ -19,6 +25,25 @@ cd "/repo" || { echo "Failed to change to repository directory"; exit 1; }
 
 # Checkout the specified branch
 git checkout "$branch" || { echo "Failed to checkout branch"; exit 1; }
+
+# Check if the docker-compose.yaml file exists
+if [ -f "docker-compose.yaml" ]; then
+    # try to stop and remove any existing containers
+    docker-compose down
+    # Run Docker Compose
+    docker-compose up -d
+    # check if stack is uo
+    # Check if the stack is up
+    if docker-compose ps | grep -q "Up"; then
+        echo "Docker Compose stack is running."
+    else
+        echo "Docker Compose stack is not running."
+        exit 1;
+    fi
+    exit 0;
+else
+    echo "docker-compose.yaml file not found. Looking to build dockerfile"
+fi
 
 # Run any additional commands or scripts here
 image_name="$repo_name-$branch"
@@ -39,7 +64,7 @@ if ! docker network inspect $network >/dev/null 2>&1; then
     docker network create $network
 fi
 
-docker run -d -p $container_port:$container_port -h $branch --name $image_name $image_name
+docker run -d -p $container_port:$container_port -h $image_name --name $image_name $image_name
 docker network connect $network $image_name
 
 # Exit successfully
